2bed3b7ee33084973826c3263bc4159936c6d751
root <root@surface-law.localdomain>
2023-04-16 00:10:48 +0800

    tmp

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,11 +6,32 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const frontendDir = path.join(__dirname, "../../frontend/dist");
 import rootRouter from "./routes";
 import { prisma } from "./adapters";
+import session from 'express-session';
 
 const app = express();
 app.use(express.static(frontendDir));
 
 app.use(express.json());
+
+
+if (process.env.NODE_ENV === "production") {
+    app.set("trust proxy", 1);
+}
+app.use(
+    session({
+        cookie: {
+            httpOnly: true,
+            sameSite: "strict",
+            secure: process.env.NODE_ENV === "production",
+            maxAge: null, // session cookie
+        },
+        // use random secret
+        name: "sessionId", // don't omit this option
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: false,
+    })
+);
 
 app.use(rootRouter);
 
